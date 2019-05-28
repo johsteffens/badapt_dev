a902c8d22babb73684b6816471a443dc2d6e2c2f
Johannes Steffens <johannes.b.steffens@gmail.com>
2019-05-28 20:17:11 +0200

    migrated recurrent networks to beth; renamed dev libray

---
+++ lib/badapt_dev/badapt_dev_ern.h
@@ -0,0 +1,48 @@
+/** Copyright 2019 Johannes Bernhard Steffens
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/** Elman Recurrent Network
+ *
+ *  Schematics (activation omitted):
+ *
+ *       vec_input---->wgt_input-----.
+ *                                  (+)-->vec_hidden-->wgt_hidden-->vec_output
+ *    .->vec_context-->wgt_context---'        |
+ *    |                                       |
+ *    '---------------------------------------'
+ *
+ */
+
+#ifndef BADAPT_DEV_ERN_H
+#define BADAPT_DEV_ERN_H
+
+#include "bcore_std.h"
+#include "badapt_activator.h"
+#include "badapt_adaptive.h"
+#include "badapt_dev_precoded.h"
+#include "badapt_ern.h"
+
+/**********************************************************************************************************************/
+// tests
+
+void badapt_dev_ern_s_test_recurrent_kjv();
+
+/**********************************************************************************************************************/
+
+vd_t badapt_dev_ern_signal_handler( const bcore_signal_s* o );
+
+/**********************************************************************************************************************/
+
+#endif // BADAPT_DEV_ERN_H
---
+++ lib/badapt_dev/badapt_dev_jrn.c
@@ -0,0 +1,103 @@
+/** Copyright 2019 Johannes Bernhard Steffens
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+#include "bcore_std.h"
+#include "bmath_std.h"
+#include "bmath_plot.h"
+#include "badapt_dev_jrn.h"
+#include "badapt_trainer.h"
+#include "badapt_dev_problem.h"
+
+/**********************************************************************************************************************/
+/// tests
+
+
+void badapt_dev_jrn_s_run_training( badapt_supplier* problem, badapt_builder* builder, const badapt_trainer_batch_s* trainer )
+{
+    BCORE_LIFE_INIT();
+    badapt_training_state* state = BCORE_LIFE_A_PUSH( badapt_trainer_batch_s_create_state( trainer ) );
+    badapt_supplier_a_setup_builder( problem, builder );
+    badapt_training_state_a_set_adaptive( state, BCORE_LIFE_A_PUSH( badapt_builder_a_build( builder ) ) );
+    badapt_training_state_a_set_supplier( state, problem );
+    badapt_training_state_a_set_guide( state, ( badapt_guide* )BCORE_LIFE_A_PUSH( badapt_guide_char_encode_s_create() ) );
+    badapt_adaptive_a_arc_to_sink( badapt_training_state_a_get_adaptive( state ), BCORE_STDOUT );
+    badapt_trainer_batch_s_run( trainer, ( badapt_training_state* )state );
+    BCORE_LIFE_RETURN();
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+void badapt_dev_jrn_s_test_recurrent_kjv()
+{
+    BCORE_LIFE_INIT();
+    BCORE_LIFE_CREATE( badapt_problem_recurrent_kjv_s, problem );
+    BCORE_LIFE_CREATE( badapt_builder_jrn_s,   builder );
+    BCORE_LIFE_CREATE( badapt_trainer_batch_s, trainer );
+
+    builder->size_hidden        = 256;
+    builder->dynamics.epsilon   = 0.01;
+    builder->dynamics.lambda_l2 = 0.00001;
+    builder->context_epsilon_factor = 0.1;
+
+//    builder->dynamics.lambda_l1 = 0.0001;
+
+    builder->activator_hidden = badapt_activator_create_from_names( "bias", "tanh" );
+    builder->activator_output = badapt_activator_create_from_names( "bias", "tanh" );
+
+    trainer->fetch_cycles_per_iteration = 10;
+    trainer->batch_cycles_per_fetch = 1;
+    trainer->max_iterations = 10;
+
+    badapt_dev_jrn_s_run_training( ( badapt_supplier* )problem, ( badapt_builder* )builder, trainer );
+    BCORE_LIFE_RETURN();
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**********************************************************************************************************************/
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+vd_t badapt_dev_jrn_signal_handler( const bcore_signal_s* o )
+{
+    switch( bcore_signal_s_handle_type( o, typeof( "badapt_jrn" ) ) )
+    {
+        case TYPEOF_init1:
+        {
+        }
+        break;
+
+        case TYPEOF_selftest:
+        {
+        }
+        break;
+
+        case TYPEOF_precoder:
+        {
+            bcore_precoder_compile( "badapt_dev_precoded", __FILE__ );
+        }
+        break;
+
+        default: break;
+    }
+
+    return NULL;
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+/**********************************************************************************************************************/
+
+
