88bc8c2d71cde22f1830e29392acfeba7337212c
Johannes Steffens <johannes.b.steffens@gmail.com>
2020-03-24 14:32:15 +0100

    removed old haptive library

--- src/main.c
+++ src/main.c
@@ -5,7 +5,6 @@
 #include "badapt_std.h"
 #include "bhvm_std.h"
 #include "badapt_dev_signal.h"
-#include "haptive_signal.h"
 #include "lion_signal.h"
 #include "badapt_dev_ern.h"
 #include "badapt_dev_lstm.h"
@@ -16,7 +15,6 @@ int main( int argc, char** argv )
     bcore_register_signal_handler( bhvm_signal_handler );
     bcore_register_signal_handler( badapt_signal_handler );
     bcore_register_signal_handler( badapt_dev_signal_handler );
-    bcore_register_signal_handler( haptive_signal_handler );
     bcore_register_signal_handler( lion_signal_handler );
 
     // bmath_quicktypes_to_stdout( NULL ); return 0;
