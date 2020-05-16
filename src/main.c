a0d8dc00aa0d52ff4ca5e488889934e0f5475487
Johannes Steffens <johannes.b.steffens@gmail.com>
2020-05-16 17:58:35 +0200

    moved library brnd to dedicated project; corrected date in main.c

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/// Author & Copyright (C) 2018 Johannes Bernhard Steffens. All rights reserved.
+/// Author & Copyright (C) 2020 Johannes Bernhard Steffens. All rights reserved.
 
 #include <sys/time.h>
 
@@ -7,7 +7,6 @@
 #include "bhpt_std.h"
 #include "badapt_dev_signal.h"
 #include "lion_signal.h"
-#include "brnd_signal.h"
 #include "badapt_dev_ern.h"
 #include "badapt_dev_lstm.h"
 
@@ -19,7 +18,6 @@ int main( int argc, char** argv )
     bcore_register_signal_handler( badapt_signal_handler );
     bcore_register_signal_handler( badapt_dev_signal_handler );
     bcore_register_signal_handler( lion_signal_handler );
-    bcore_register_signal_handler( brnd_signal_handler );
 
     // bmath_quicktypes_to_stdout( NULL ); return 0;
 
@@ -52,48 +50,6 @@ int main( int argc, char** argv )
         return ret;
     }
 
-//    bcore_get_traits_of_ancestor_to_sink( typeof( "bsym_op1" ), BCORE_STDOUT );
-
-//    bsym_test();
-//    bhgp_test();
-//    bhgp_adaptive_test();
-//      bhgp_simple_eval();
-
-//    bcore_quicktypes_to_stdout( NULL );
-//    return 0;
-
-
-
-    //activator_test();
-    //virtual_test();
-    //return 0;
-
-    badapt_mlp_s_test_sine_random();
-//    badapt_mlp_s_test_binary_add();
-//    badapt_mlp_s_test_binary_mul();
-//    badapt_mlp_s_test_binary_xsg3();
-//    badapt_mlp_s_test_binary_hash();
-//    badapt_mlp_s_test_polynom();
-
-//    badapt_c1d_s_test_sine_random();
-//    badapt_c1d_s_test_binary_add();
-//    badapt_c1d_s_test_binary_mul();
-//    badapt_c1d_s_test_binary_xsg3();
-//    badapt_c1d_s_test_binary_hash();
-//    badapt_c1d_s_test_polynom();
-
-//    CPU_TIME_TO_STDOUT( badapt_dev_lstm_test_recurrent_abc() );
-//    CPU_TIME_TO_STDOUT( badapt_lstm_test_recurrent_abc() );
-//    CPU_TIME_TO_STDOUT( badapt_ern_test_recurrent_abc() );
-
-//      CPU_TIME_TO_STDOUT( badapt_dev_lstm_test_recurrent_kjv() );
-
-    //CPU_TIME_TO_STDOUT( bcore_run_signal_selftest( typeof( "badapt_mlp" ), NULL ) );
-    //CPU_TIME_TO_STDOUT( bcore_run_signal_selftest( typeof( "bmath_adaptive_mlp" ), NULL ) );
-
-    // bcore_run_signal_selftest( typeof( "bmath_hf3" ), NULL );
-
-    //snn_selftest2();
     BLM_DOWN();
 
     bcore_down( true );
